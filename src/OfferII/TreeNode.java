54f6461d6269510d0645c144cf9f20fff0fb9702
ygq <3421325044@qq.com>
2022-04-27 15:48:48 +0800

    树结构定义

---
+++ src/OfferII/TreeNode.java
@@ -0,0 +1,19 @@
+package OfferII;
+
+/**
+ * @author yanggq
+ * @create 2022-04-27 15:02
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+
+}
