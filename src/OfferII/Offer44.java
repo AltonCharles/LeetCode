274094bf0da50dfcf19bc8c5398c566c2cc6efea
ygq <3421325044@qq.com>
2022-04-29 19:49:26 +0800

    递归每层最大值

---
+++ src/OfferII/Offer44.java
@@ -0,0 +1,36 @@
+package OfferII;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author yanggq
+ * @create 2022-04-29 18:54
+ */
+/*给定一棵二叉树的根节点 root ，请找出该二叉树中每一层的最大值。
+* */
+public class Offer44 {
+    public List<Integer> largestValues(TreeNode root) {
+        int LMax=0;
+        Queue<TreeNode> queue=new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        if(root==null)
+            return list;
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            for(int i=queue.size();i>0;i--){
+                TreeNode temp=queue.peek();
+                LMax=Math.max(LMax,temp.val);
+                queue.poll();
+                if(temp.left!=null)
+                    queue.offer(temp.left);
+                if(temp.right!=null)
+                    queue.offer(temp.right);
+            }
+            list.add(LMax);
+        }
+        return list;
+    }
+}
