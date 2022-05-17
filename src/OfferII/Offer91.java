aa7a00b1b486e3745b501683462b446ff16a9044
ygq <3421325044@qq.com>
2022-05-17 12:47:52 +0800

    动态规划

---
+++ src/OfferII/Offer91.java
@@ -0,0 +1,35 @@
+package OfferII;
+
+/**
+ * @author yanggq
+ * @create 2022-05-17 12:32
+ */
+/*
+* 假如有一排房子，共 n 个，每个房子可以被粉刷成红色、蓝色或者绿色这三种颜色中的一种，
+* 你需要粉刷所有的房子并且使其相邻的两个房子颜色不能相同。
+当然，因为市场上不同颜色油漆的价格不同，所以房子粉刷成不同颜色的花费成本也是不同的。
+* 每个房子粉刷成不同颜色的花费是以一个 n x 3 的正整数矩阵 costs 来表示的。
+例如，costs[0][0] 表示第 0 号房子粉刷成红色的成本花费；
+* costs[1][2] 表示第 1 号房子粉刷成绿色的花费，以此类推。
+请计算出粉刷完所有房子最少的花费成本。
+*
+* */
+public class Offer91 {
+    public int minCost(int[][] costs) {
+        int row = costs.length;
+        int[][] dp = new int[row][3];
+        dp[0][0] = costs[0][0];
+        dp[0][1] = costs[0][1];
+        dp[0][2] = costs[0][2];
+        for (int i = 1; i < row; i++) {
+            dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];
+            dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];
+            dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];
+        }
+        return min(dp[row-1][0],dp[row-1][1],dp[row-1][2]);
+    }
+
+    public int min(int a,int b,int c){
+        return Math.min(Math.min(a,b),c);
+    }
+}
