0e7cb01bccf1ec4d8a4a204d841bd8ef8d741363
ygq <3421325044@qq.com>
2022-05-26 19:47:13 +0800

    双指针

---
+++ src/OfferII/Offer23.java
@@ -0,0 +1,20 @@
+package OfferII;
+
+/**
+ * @author yanggq
+ * @create 2022-05-26 19:19
+ */
+public class Offer23 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+
+}
